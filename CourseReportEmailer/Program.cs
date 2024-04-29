6213b5cc950bb6e5d54f6695951f42b74856ed45
Ahmet <57807015+ahmet-cigil@users.noreply.github.com>
2024-04-29 17:15:11 +0200

    Add project files.

---
+++ CourseReportEmailer/Program.cs
@@ -0,0 +1,69 @@
+﻿using CourseReportEmailer.Models;
+using CourseReportEmailer.Repository;
+using CourseReportEmailer.Worker;
+using Newtonsoft.Json;
+using System;
+using System.Data;
+
+namespace CourseReportEmailer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                EnrollmentDetailReportCommand command = new EnrollmentDetailReportCommand(@"Data Source=thinkbook14g6\mssqlserver01;Initial Catalog=CourseReport;Integrated Security=True;Encrypt=True");
+                IList<EnrollmentDetailReportModel> models = command.GetList();
+
+                var reportFileName = "EnrollmentDetailsReport.xlsx";
+                EnrollmentDetailReportSpreadSheetCreator enrollmentSheetCreator = new EnrollmentDetailReportSpreadSheetCreator();
+                enrollmentSheetCreator.Create(reportFileName, models);
+
+                EnrollmentDetailReportEmailSender emailer = new EnrollmentDetailReportEmailSender();
+                emailer.Send(reportFileName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong: {0}", ex.Message);
+            }
+
+            //EnrollmentDetailReportModel model = new EnrollmentDetailReportModel()
+            //{
+            //    EnrollmentId = 1,
+            //    FirstName = "Mark",
+            //    LastName = "Twain",
+            //    CourseCode = "CA",
+            //    Description = "..."
+            //};
+
+            //var json = JsonConvert.SerializeObject(model);
+            //EnrollmentDetailReportModel objectFromJson = 
+            //    (EnrollmentDetailReportModel)JsonConvert.DeserializeObject(json, typeof(EnrollmentDetailReportModel));
+
+            //DataTable table = new DataTable();
+
+            //DataColumn column1 = new DataColumn("EnrollmentId", typeof(int));
+            //DataColumn column2 = new DataColumn("FirstName", typeof(string));
+            //DataColumn column3 = new DataColumn("LastName", typeof(string));
+            //DataColumn column4 = new DataColumn("CourseCode", typeof(string));
+            //DataColumn column5 = new DataColumn("Description", typeof(string));
+
+            //table.Columns.Add(column1);
+            //table.Columns.Add(column2);
+            //table.Columns.Add(column3);
+            //table.Columns.Add(column4);
+            //table.Columns.Add(column5);
+
+            //table.Rows.Add(true, "Mark", "Twain", 7, "...");
+
+            //foreach (DataRow row in table.Rows)
+            //{
+            //    foreach (DataColumn column in table.Columns)
+            //    {
+            //        Console.WriteLine(row[column]);
+            //    }
+            //}
+        }
+    }
+}
---
+++ CourseReportEmailer/Worker/EnrollmentDetailReportEmailSender.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseReportEmailer.Worker
+{
+    class EnrollmentDetailReportEmailSender
+    {
+        public void Send(string fileName)
+        {
+            SmtpClient client = new SmtpClient();
+            client.Port = 587;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+
+            NetworkCredential creds = new NetworkCredential("mark.twain@email.com", "password");
+            client.EnableSsl = true;
+            client.Credentials = creds;
+
+            MailMessage message = new MailMessage("mark.twain@email.com", "twain.mark@email.com");
+            message.Subject = "Enrollment Details Report";
+            message.IsBodyHtml = true;
+            message.Body = "Hi, <br><br>Attached please find the enrolment details report<br><br>";
+
+            Attachment attachment = new Attachment(fileName);
+            message.Attachments.Add(attachment);
+            client.Send(message);
+        }
+    }
+}
